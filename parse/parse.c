db1ab3b009618e563ab9232a465335f37e19f10c
Lorenzo Di mascia <ldi-masc@e3r12p6.42roma.it>
2023-01-18 11:50:14 +0100

    checkare le quote e virgolette

--- parse/parse.c
+++ parse/parse.c
@@ -30,7 +30,7 @@ int	check_pipe(char *string)
 {
 	size_t i;
 	i = ft_strlen(string);
-	if(i > 1 && string[1] == '|')
+	if(i > 1)
 	{
 		printf("double pipe is not alowed");
 		return(1);
@@ -42,7 +42,7 @@ int	check_redirection(char *string)
 {
 	size_t i;
 	i = ft_strlen(string);
-	if(i > 2 && string[0] == string[1])
+	if(i > 2)
 	{
 		printf("no more then 2 redirection");
 		return(1);
@@ -55,6 +55,23 @@ int	check_redirection(char *string)
 	return (0);
 }
 
+int check_quote(char *string, char c)
+{
+	size_t i;
+	i = ft_strlen(string);
+	if (i == 2)
+	{
+		printf("command not found");
+		return(1);
+	}
+	if((string[0] == 34 && string[i - 1] != 34) && (string[0] == 39 && string[i - 1] != 39))
+	{
+		printf("quote not closed");
+		return(1);
+	}
+	return(0);
+}
+
 int	check_parameter(char *string, char c)
 {
 	if(c == '|')
@@ -62,6 +79,9 @@ int	check_parameter(char *string, char c)
 			return (1);
 	if(c == '<' || c == '>')
 		if(check_redirection(string))
+			return(1);
+	if(c == 34 || c == 39)
+		if(check_quote(string, c))
 			return(1);
 	return (0);
 }
@@ -75,10 +95,10 @@ int	parse(char **string)
 		printf("il carattere | non può essere all'inizio dell'array");
 		return 1;
 	}
-	i = 2;
+	i = 1;
 	while (string[i])
 	{
-		if(string[i][0] == '|' || string[i][0] == '<' || string[i][0] == '>')
+		if(string[i][0] == '|' || string[i][0] == '<' || string[i][0] == '>' || string[i][0] == 34 || string[i][0] == 39)
 			if(check_parameter(string[i], string[i][0]))
 				return(1); 
 		i++;
@@ -89,8 +109,10 @@ int	parse(char **string)
 int main (int ac, char **av)
 {
 	int i;
+	int j;
 
 	i = 1;
+	j = 0;
 	if(parse(av))
 		return 1;
 }
