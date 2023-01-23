93a17373922231612e8a9953724b18e6eabacb51
Michela Volpi <mvolpi@e3r8p8.42roma.it>
2023-01-23 11:47:23 +0100

    aggiunto commenti

--- src/split/split_executor.c
+++ src/split/split_executor.c
@@ -6,12 +6,21 @@
 /*   By: mvolpi <mvolpi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/23 11:20:12 by mvolpi            #+#    #+#             */
-/*   Updated: 2023/01/23 11:24:47 by mvolpi           ###   ########.fr       */
+/*   Updated: 2023/01/23 11:43:10 by mvolpi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "split.h"
 
+/**
+ * @brief Takes a single character and checks if it is the one of the
+ * separators, it returns a diffirent number depending on which operator
+ * it is
+ * 
+ * @param c the character to be checked
+ * @return int it returns a diffirent number depending on which operator
+ * it is
+ */
 int	separator(char c)
 {
 	if (c == '\0')
@@ -29,6 +38,13 @@ int	separator(char c)
 	return (0);
 }
 
+/**
+ * @brief Scrolls the entire string and count into how many strings
+ * the sent string needs to be divided into
+ * 
+ * @param str the string that is to be divided
+ * @return int how many strings the sent string needs to be divided into
+ */
 int	how_much_words(char *str)
 {
 	int	i;
@@ -46,6 +62,13 @@ int	how_much_words(char *str)
 	return (w);
 }
 
+/**
+ * @brief takes the string that needs to be save and the array where
+ * we save the string, save character by charater
+ * 
+ * @param dest cell of the string array
+ * @param src string that needs to be save
+ */
 void	word(char *dest, char *src)
 {
 	int	i;
@@ -59,6 +82,20 @@ void	word(char *dest, char *src)
 	dest[i] = '\0';
 }
 
+/**
+ * @brief Takes the string array and the string that needs to be split
+ * with a loop it scroll through the whole string and checks letter by
+ * letter, with the is_separator function it checks if str[i] is a
+ * separator, if it skip the separator. If instead it is not one of the
+ * separators with a loop it counts how long the string is and
+ * mallocates the cell of the array for that lenght and then with
+ * word saves the whole string up to the next separator or ends at
+ * the end
+ * 
+ * @param split the string array where we saves the splitted string
+ * @param str the string that needs to be split
+ * @return int return 1
+ */
 int	split(char **split, char *str)
 {
 	int	i;
@@ -87,6 +124,15 @@ int	split(char **split, char *str)
 	return (1);
 }
 
+/**
+ * @brief Takes the string to be split and counts the cells of the array
+ * to be prepared where to insert the single strings to be created, once
+ * the count is finished it mallocates the necessary cells and checks
+ * that if the mallocation or copying fails it frees everything
+ * 
+ * @param s the string that needs to be split
+ * @return char** the string array with split string
+ */
 char	**split_executor(char const *s)
 {
 	int		w;
