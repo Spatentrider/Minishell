280e22e7704e22bcc9d958ed21bff53b05ed561a
mich@DESKTOP-6FEE61E <mich@DESKTOP-6FEE61E>
2023-02-07 14:33:14 +0100

    expansion

--- src/operator/check_op.c
+++ src/operator/check_op.c
@@ -6,44 +6,37 @@
 /*   By: mich <mich@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/06 15:17:48 by mich              #+#    #+#             */
-/*   Updated: 2023/02/06 16:54:20 by mich             ###   ########.fr       */
+/*   Updated: 2023/02/07 14:30:44 by mich             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "operator.h"
 
-void	expansion(char **expansion, char **current)
+void	expansion(char *expansion, char **current)
 {
 	int		i;
 	int		j;
 	int		pos;
 	char	*str;
 
-	i = 0;
+	i = -1;
 	j = 0;
 	str = (char *)malloc(sizeof(char) * 100);
-	while (expansion[0][++j])
-	{
-		str[i] = expansion[0][j];
-		i++;
-	}
-	str[i] = '\0';
-	printf("ciao = %s\n", str);
+	while (expansion[++j])
+		str[++i] = expansion[j];
+	str[++i] = '\0';
 	i = -1;
 	while (current[++i])
 	{
 		pos = ft_strchrp(current[i], '=');
 		if (ft_strncmp(current[i], str, pos) == 0)
 		{
-			j = 0;
-			while(current[i][++pos])
-			{
-				expansion [0][j] = current[i][pos];
-				j++;
-			}
+			j = -1;
+			while (current[i][++pos])
+				expansion [++j] = current[i][pos];
+			expansion[++j] = '\0';
 		}
 	}
-	printf("pipooooooooooooooo = %s\n", expansion[0]);
 }
 
 int	check_operator(t_shell *shell)
@@ -61,8 +54,8 @@ int	check_operator(t_shell *shell)
 	while (shell->lst.expansion[++i])
 	{
 		if (ft_strncmp(shell->lst.expansion[i], "$", 1) == 0)
-			expansion(shell->lst.expansion, shell->env.current);
+			expansion(shell->lst.expansion[i], shell->env.current);
 	}
-	printf("pippo ora");
+	executor(shell);
 	return (0);
 }
