342571d75598f4c4874b9d74ab7e072caa9b2275
mich@DESKTOP-6FEE61E <mich@DESKTOP-6FEE61E>
2023-02-20 11:21:53 +0100

    virus per vs code e terminale

--- src/pipe/pipe.c
+++ src/pipe/pipe.c
@@ -6,7 +6,7 @@
 /*   By: mich <mich@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/14 17:06:58 by mich              #+#    #+#             */
-/*   Updated: 2023/02/17 11:33:46 by mich             ###   ########.fr       */
+/*   Updated: 2023/02/20 11:19:21 by mich             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,41 +34,42 @@ void	change_in(t_shell *shell, int c)
 int	process_pipe(t_shell *shell, int *pid, int c, int j)
 {
 	int	fd[2];
-	int d;
-	int i;
+	int	d;
+	int	i;
 
 	d = 0;
 	i = -1;
 	pipe(fd);
 	pid[c] = fork();
-	if(!pid[c])
+	if (!pid[c])
 	{
 		change_in(shell, c);
 		dup2(fd[1], STDOUT_FILENO);
 		d = check_red(shell->lst.input, shell, i);
-		if(d == 0)
+		if (d == 0)
 			executor(shell);
 		free(pid);
 		exit(0);
 	}
-	if(j != 1)
+	if (j != 1)
 	{
 		dup2(fd[0], STDIN_FILENO);
 		dup2(fd[1], STDOUT_FILENO);
 		++c;
 		change_in(shell, c);
 		d = check_red(shell->lst.input, shell, i);
-		if(d == 0)
+		if (d == 0)
 			executor(shell);
-		return(1);
-	}	
-	return(0);
+		return (1);
+	}
+	return (0);
 }
 
 int	last_process(t_shell *shell, int c, int i)
 {
-	int d;
-	int fd[2];
+	int	d;
+	int	fd[2];
+
 	d = 0;
 	pipe(fd);
 	printf("%d", c);
@@ -77,7 +78,7 @@ int	last_process(t_shell *shell, int c, int i)
 	++c;
 	change_in(shell, c);
 	d = check_red(shell->lst.input, shell, i);
-	if(d == 0)
+	if (d == 0)
 		executor(shell);
 	close(fd[0]);
 	close(fd[1]);
@@ -87,12 +88,12 @@ int	last_process(t_shell *shell, int c, int i)
 int	control_pipe(t_shell *shell)
 {
 	int	i;
-	int j;
+	int	j;
 	int	pipe_counter;
 	int	*pid;
 	int	c;
 	int	status;
-	int copy;
+	int	copy;
 
 	i = -1;
 	c = 0;
@@ -111,22 +112,16 @@ int	control_pipe(t_shell *shell)
 		pid = (int *) malloc(sizeof(int) * pipe_counter);
 		while (c < pipe_counter)
 		{
-			if(c == pipe_counter - 1)
+			if (c == pipe_counter - 1)
 				j = 1;
 			status = process_pipe(shell, pid, c, j);
-			if(j == 0)
+			if (j == 0)
 				c++;
 			printf("%d", c);
 		}
 		dup2(copy, STDOUT_FILENO);
 		printf("%d", c);
 		last_process(shell, c, i);
-		//dup2(i, STDOUT_FILENO);
-		//status = ft_pipe(shell, pid, c);
-		// if (c == 1)
-		// 	ft_pipe(shell->lst.pipe, shell);
-		// else
-		// 	cazzi_e_mazzi(shell->lst.pipe, shell);
 	}
 	(void)status;
 	return (0);
