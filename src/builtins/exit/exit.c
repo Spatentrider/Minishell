2a3318d64ccb7f168fa29811d40fd26502012941
mich@DESKTOP-6FEE61E <mich@DESKTOP-6FEE61E>
2023-02-20 17:55:07 +0100

    bho

--- src/builtins/exit/exit.c
+++ src/builtins/exit/exit.c
@@ -6,39 +6,47 @@
 /*   By: mich <mich@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/01 15:17:02 by mich              #+#    #+#             */
-/*   Updated: 2023/02/09 16:59:24 by mich             ###   ########.fr       */
+/*   Updated: 2023/02/20 17:54:40 by mich             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "exit.h"
 
+void	nor_exit(t_shell *shell)
+{
+	int	k;
+
+	k = -1;
+	if (shell->lst.executor[1] != NULL)
+	{
+		while (shell->lst.executor[1][++k])
+		{
+			if (isdigit(shell->lst.executor[1][k]) == 0)
+			{
+				printf("exit\n");
+				exit(printf("minishell: exit: %s: numeric argument required\n", \
+					shell->lst.executor[1]));
+			}
+		}
+	}
+	exit(printf("exit\n"));
+}
+
 void	ft_exit(char **current, t_shell *shell)
 {
 	int		i;
-	int		j;
-	int		k;
 
 	i = -1;
-	j = -1;
-	k = -1;
 	while (current[++i])
 	{
-		if (strncmp(current[i], "SHLVL=", 6) == 0)
-		{
-
-			j = current[i][6] - '0';
-			j--;
-			j += '0';
-			current[i][6] = j;
-			if(shell->lst.executor[1] != NULL)
-			{
-				while(isdigit(shell->lst.executor[1][++k]) == 0)
-				{
-					printf("exit\n");
-					exit(printf("minishell: exit: %s: numeric argument required\n", shell->lst.executor[1]));
-				}
-			}
-			exit(printf("exit\n"));
-		}
+		if (strncmp(current[i], "SHLVL=3", 7) == 0)
+			nor_exit(shell);
 	}
 }
+
+// int		j;
+// j = -1;
+// j = current[i][6] - '0';
+// j--;
+// j += '0';
+// current[i][6] = j;
