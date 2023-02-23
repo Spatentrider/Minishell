2fa56226eab42452f1390083d3e6272fdaa2cfba
root <root@DESKTOP-JTF0QFN.localdomain>
2023-02-23 15:48:52 +0100

    yeee

--- src/builtins/cd/cd.c
+++ src/builtins/cd/cd.c
@@ -14,20 +14,28 @@
 
 void	ft_cd(t_shell *shell)
 {
-	shell->cd.i = -1;
-	shell->cd.oldpwd = ft_pwd();
-	shell->cd.s = chdir(shell->lst.executor[1]);
-	while (shell->env.current[++shell->cd.i])
+	if (shell->lst.executor[1] == NULL)
 	{
-		if (strncmp(shell->env.current[shell->cd.i], "OLDPWD", 6) == 0)
+		chdir("/root");
+	}
+	else
+	{
+		shell->cd.i = -1;
+		shell->cd.oldpwd = ft_pwd();
+		shell->cd.s = chdir(shell->lst.executor[1]);
+		while (shell->env.current[++shell->cd.i])
 		{
-				shell->cd.pwd = ft_strjoin("OLDPWD=", shell->cd.oldpwd);
-				shell->env.current[shell->cd.i] = shell->cd.pwd;
-		}
-		if (strncmp(shell->env.current[shell->cd.i], "PWD", 3) == 0)
-		{
-			shell->cd.pwd2 = ft_strjoin("PWD=", ft_pwd());
-			shell->env.current[shell->cd.i] = shell->cd.pwd2;
+			if (strncmp(shell->env.current[shell->cd.i], "OLDPWD", 6) == 0)
+			{
+					shell->cd.pwd = ft_strjoin("OLDPWD=", shell->cd.oldpwd);
+					shell->env.current[shell->cd.i] = shell->cd.pwd;
+			}
+			if (strncmp(shell->env.current[shell->cd.i], "PWD", 3) == 0)
+			{
+				shell->cd.pwd2 = ft_strjoin("PWD=", ft_pwd());
+				shell->env.current[shell->cd.i] = shell->cd.pwd2;
+			}
 		}
 	}
 }
+
