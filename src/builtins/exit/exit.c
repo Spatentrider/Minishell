c4412fda5cd8cfedd1624cb9c10adf907c52f201
Mich120201 <volpimichela3@gmail.com>
2023-04-15 15:03:01 +0200

    norm exit

--- src/builtins/exit/exit.c
+++ src/builtins/exit/exit.c
@@ -6,11 +6,26 @@
 /*   By: mich <mich@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/01 15:17:02 by mich              #+#    #+#             */
-/*   Updated: 2023/03/16 11:33:29 by mich             ###   ########.fr       */
+/*   Updated: 2023/04/15 15:02:43 by mich             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "exit.h"
+
+void	printf_error(t_shell *shell)
+{
+	if (isdigit(shell->lst.executor[1][k]) == 0)
+	{
+		if (shell->lst.executor[2] == NULL)
+		{
+			printf("exit\n");
+			printf("minishell: exit: %s: numeric argument required\n", \
+			shell->lst.executor[1]);
+			free_struct(shell);
+			exit(1);
+		}
+	}
+}
 
 void	ft_exit(t_shell *shell)
 {
@@ -25,19 +40,7 @@ void	ft_exit(t_shell *shell)
 	if (shell->lst.executor[1] != NULL)
 	{
 		while (shell->lst.executor[1][++k])
-		{
-			if (isdigit(shell->lst.executor[1][k]) == 0)
-			{
-				if (shell->lst.executor[2] == NULL)
-				{
-					printf("exit\n");
-					printf("minishell: exit: %s: numeric argument required\n", \
-					shell->lst.executor[1]);
-					free_struct(shell);
-					exit(1);
-				}
-			}
-		}
+			printf_error(shell);
 		printf("exit\n");
 		k = ft_atoi(shell->lst.executor[1]);
 		free_struct(shell);
